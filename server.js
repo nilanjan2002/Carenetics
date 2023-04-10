f2cbb4fc292a5aed337db01f0de98d58166061a8
Nilanjan Sengupta <nilanjansengupta471@gmail.com>
2023-04-11 01:06:52 +0530

    Origin

---
+++ server.js
@@ -0,0 +1,14 @@
+//create an express server to handle all the server requests , use port as 3000
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3001;
+const base = `${__dirname}/index.html`
+
+app.use(express.static(__dirname));
+app.get('/',(req,res)=>{
+    res.sendFile(base);
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on ${port}`);
+})
